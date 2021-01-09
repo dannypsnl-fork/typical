09fe1360dc464f18ac015df5038d5170e527a2fd
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-01-09 08:43:13 +0800

    [fix] remove claim

--- backend.rkt
+++ backend.rkt
@@ -1,25 +1,22 @@
 #lang racket
 
+(provide (all-defined-out))
+
 (require nanopass/base
          "lang.rkt")
+
+(define-pass pass:remove-claim : Typical (s) -> * ()
+  (Stmt : Stmt (s) -> * ()
+        [(claim ,stx ,name ,typ) #f]
+        [else s])
+  (Stmt s))
 
 (define-language L1
   (extends Typical)
   (Stmt ()
-        (- (claim stx name typ)
-           (data stx name (dependency* ...) constructor* ...))))
+        (- (claim stx name typ))))
 
-(define-pass pass:remove-type-related : Typical (s) -> L1 ()
+(define-pass pass:Typical->L1 : Typical (s) -> L1 ()
   (Stmt : Stmt (s) -> Stmt ()
-        [(claim ,stx ,name ,typ) (error 'unreachable)]
-        [(data ,stx ,name (,dependency* ...) ,constructor* ...)
-         (error 'unreachable)])
-  (Stmt s))
-
-(define-pass pass:to-racket : L1 (s) -> * ()
-  (Stmt : Stmt (s) -> * ()
-        [(is-a? ,stx ,expr ,typ)
-         expr]
-        [(define ,stx ,name ,expr)
-         `(define ,(syntax-e name) ,expr)])
+        [(claim ,stx ,name ,typ) (raise-syntax-error 'unreachable "internal bug" stx)])
   (Stmt s))
