7a544c751690fc9cc566ed3296f39b58c5039f1f
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2021-01-16 22:39:04 +0800

    improve parser, using qualifier

--- private/lang.rkt
+++ private/lang.rkt
@@ -49,41 +49,40 @@
   (Stmt : * (stx) -> Stmt (stmt)
         (syntax-parse stx
           #:datum-literals (data :? : =)
-          [(data (name dep* ...) constructor* ...)
+          [(data (name:id dep* ...) constructor* ...)
            `(data ,stx ,#'name (,(map Bind (syntax->list #'(dep* ...))) ...)
                   ,(map Bind (syntax->list #'(constructor* ...))) ...)]
-          [(data name constructor* ...)
+          [(data name:id constructor* ...)
            `(data ,stx ,#'name ()
                   ,(map Bind (syntax->list #'(constructor* ...))) ...)]
-          [(expr :? typ)
+          [(expr:expr :? typ)
            `(is-a? ,stx ,(Expr #'expr) ,(Type #'typ))]
-          [(name : typ)
+          [(name:id : typ)
            `(claim ,stx ,#'name ,(Type #'typ))]
-          [(name = expr)
+          [(name:id = expr:expr)
            `(define ,stx ,#'name ,(Expr #'expr))]
           [else (wrong-syntax stx "invalid statement")]))
   (Bind : * (stx) -> Bind (constructor)
-        (syntax-case stx (:)
-          [(name* ... : typ)
+        (syntax-parse stx
+          #:datum-literals (:)
+          [(name*:id ... : typ)
            `(: ,(syntax->list #'(name* ...)) ... ,(Type #'typ))]
           [else (wrong-syntax stx "bad binding")]))
   (Expr : * (stx) -> Expr (expr)
         (syntax-parse stx
           #:datum-literals (λ match)
-          [(λ (param* ...) exp)
+          [(λ (param*:id ...) exp:expr)
            `(λ ,stx (,(map Expr (syntax->list #'(param* ...))) ...) ,(Expr #'exp))]
           [(match {exp* ...} clause* ...)
            `(match ,stx (,(map Expr (syntax->list #'(exp* ...))) ...)
               ,(map Clause (syntax->list #'(clause* ...))) ...)]
-          [(match exp clause* ...)
+          [(match exp:expr clause* ...)
            `(match ,stx (,(Expr #'exp))
               ,(map Clause (syntax->list #'(clause* ...))) ...)]
-          [(f arg* ...)
+          [(f:expr arg*:expr ...)
            `(app ,stx ,(Expr #'f) ,(map Expr (syntax->list #'(arg* ...))) ...)]
-          [_
-           (cond
-             [(identifier? stx) stx]
-             [else (wrong-syntax stx "invalid expression")])]))
+          [e:id #'e]
+          [else (wrong-syntax stx "invalid expression")]))
   (Clause : * (stx) -> Clause (clause)
           (syntax-parse stx
             #:datum-literals (=>)
@@ -103,15 +102,14 @@
              [name:id #'name]
              [else (wrong-syntax stx "invalid pattern")]))
   (Type : * (stx) -> Type (typ)
-        (syntax-case stx (->)
+        (syntax-parse stx
+          #:datum-literals (->)
           [(typ* ... -> typ)
            `(-> ,(map Type (syntax->list #'(typ* ...))) ... ,(Type #'typ))]
           [(typ* ...)
            `(,(map Type (syntax->list #'(typ* ...))) ...)]
-          [_
-           (cond
-             [(identifier? stx) (syntax-e stx)]
-             [else (wrong-syntax stx "invalid type")])]))
+          [ty:id (syntax->datum #'ty)]
+          [else (wrong-syntax stx "invalid type")]))
   (Stmt stx))
 
 (define-pass exp->stx : Typical (e) -> * ()
